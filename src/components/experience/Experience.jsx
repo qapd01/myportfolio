a53c1b093437f6c96f638f8570be6239bba68690
Sorawid Mingmolee <bossxlp@gmail.com>
2022-08-19 13:43:06 +0700

    adjust experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -9,37 +9,39 @@ import { Container,Row,Col } from 'reactstrap'
 const developmentExperienceData = [
     {
         year : '2021-2022',
-        title: 'Emergency Medical Technician Volunteer (Sirikit Hospital)',
-        desc : 'Teaching basic medical first aid, Administer a COVID-19 vaccine injection,Medical first aid on site'
+        title: 'Emergency Medical Technician Volunteer ',
+        place : 'Sirikit Hospital',
+        // desc : 'Teaching basic medical first aid, Administer a COVID-19 vaccine injection,Medical first aid on site'
     },
     {
         year : '2020-2021',
         title: 'Junior Officer (Precious Shipping Company )',
-        desc : 'Keeping of cargo watch in port thereby assisting in cargo operations, Maintenance and upkeep of all deck portable fire fighting equipment and lifesaving appliances, Preparing schedules of training and drills under the guidance of chief officer'
+        // desc : 'Keeping of cargo watch in port thereby assisting in cargo operations, Maintenance and upkeep of all deck portable fire fighting equipment and lifesaving appliances, Preparing schedules of training and drills under the guidance of chief officer'
     },
     {
         year : '2019-2020',
         title: 'Cargo Surveyor (Intertek Testing & Inspection Service)',
-        desc : "Inspect cargo on seagoing vessels,Take sample from cargo"
+        // desc : "Inspect cargo on seagoing vessels,Take sample from cargo"
     },
-]
-
-const developmentExperienceData1 = [
     {
         year : '2018-2019',
         title: 'Cadet (Precious Shipping Company )',
-        desc : 'Taking charge of mooring and unmooring operation, Preparation of port papers for master, Maintaining stability by carrying out ballast and de-ballasting operation, Maintaining inventory of bonded stores, Assist in planning, coordinating and executing of tributes to combat readiness by promoting efficient communication'
+        // desc : 'Taking charge of mooring and unmooring operation, Preparation of port papers for master, Maintaining stability by carrying out ballast and de-ballasting operation, Maintaining inventory of bonded stores, Assist in planning, coordinating and executing of tributes to combat readiness by promoting efficient communication'
     },
     {
         year : '2017-2018',
         title: 'Cargo Surveyor (Intertek Testing & Inspection Service)',
-        desc : 'Inspect cargo on seagoing vessels,Take sample from cargo'
+        // desc : 'Inspect cargo on seagoing vessels,Take sample from cargo'
     },
     {
         year : '2016-2017',
         title: 'Merchant',
-        desc : 'Selling Kids clothes and woman clothes'
+        // desc : 'Selling Kids clothes and woman clothes'
     },
+]
+
+const developmentExperienceData1 = [
+   
 ]
 
 const Experience = () => {
@@ -50,7 +52,7 @@ const Experience = () => {
                 <h2>Experience</h2>
             </Col>
 
-            <Col lg='6' md='6'>
+            <Col lg='12' md='12'>
                 <div className="single__experience-container">
                     {
                         developmentExperienceData.map((item,index) =><div 
@@ -60,27 +62,13 @@ const Experience = () => {
                         </span>
                         <h6>{item.year}</h6>
                        <h5>{item.title}</h5>
+                       <p>{item.place}</p>
                        <p>{item.desc}</p>
                     </div>)
                     }
                 </div>
             </Col>
 
-            <Col lg='6'  md='6'>
-                <div className="single__experience-container">
-                    {
-                        developmentExperienceData1.map((item,index) =><div 
-                        className="single__experience" key={index}>
-                        <span className='experience__icon'>
-                            <i class="ri-briefcase-line"></i>
-                        </span>
-                        <h6>{item.year}</h6>
-                       <h5>{item.title}</h5>
-                       <p>{item.desc}</p>
-                    </div>)
-                    }
-                </div>
-            </Col>
         </Row>
     </Container>
   </section>
